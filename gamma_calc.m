af8ab82f23a47b3ac99bf01c0314d7a43c659ce4
Tanmay Pathak <tanmay.pathak@students.iiit.ac.in>
2021-12-01 18:08:42 +0400

    remove duplicates

--- gamma_calc.m
+++
@@ -1,5 +0,0 @@
-%% FUNCTION TO CALCULATE GAMMA: PARAMETERS ARE USELESS AT THE MOMENT
-function gamma = gamma_calc(p_tot, alpha, k, l, noise_power)
-    gamma = k * p_tot * (l^(-alpha));
-    gamma = gamma / noise_power;
-end
--- rho_calc.m
+++
@@ -1,7 +0,0 @@
-%% CALCULATE ALL RHO'S ASSUMING Y = 6 IS REFLECTOR
-
-function [rho12, rho1_2, rho12_] = rho_calc(a, b, c, lambda)
-    rho12_ = exp(-lambda * (a + b)) * (1 - exp(-lambda * b));
-    rho1_2 = exp(-lambda * (b + c)) * (1 - exp(-lambda * a));
-    rho12 = exp(-lambda * (a + b + c));
-end
